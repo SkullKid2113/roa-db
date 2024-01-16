834bdd30df8f4d9da0b73fb3595f80b7e4f40327
Bill Vasfaret <125101202+SkullKid2113@users.noreply.github.com>
2024-01-15 19:23:38 -0600

    whadafuq

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,20 +1,23 @@
-import {Router, Request, Response} from "express";
+import { Router, Request, Response } from "express";
 import db from "./db";
-import {Rules} from "../entity/rules";
+import { Rules } from "../entity/rules";
+import { Like } from "typeorm";
 
-const router = Router()
+const router = Router();
 
 router.get("/search", (req: Request, res: Response) => {
-  console.log("/search request: ", req.query)
-  res.json(req.query)
-})
+  console.log("/search request: ", req.query);
+  const searchLike = dataSource.getRepository([Rules]).findBy({
+    title: Like("%no %"),
+  });
+  res.json(req.query);
+});
 
 router.get("/rules", (req: Request, res: Response) => {
-
   db.query("SELECT * FROM rules").then((rules: Rules[]) => {
-    console.log("# rules loaded", rules.length)
-    res.json(rules)
-  })
-})
+    console.log("# rules loaded", rules.length);
+    res.json(rules);
+  });
+});
 
-export default router
+export default router;
