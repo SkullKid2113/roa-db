e89c875c63035dd739e443f1f215cf322b3ccbdb
Bill Vasfaret <125101202+SkullKid2113@users.noreply.github.com>
2024-01-18 15:06:52 -0600

    colors

--- src/app/controller/api.ts
+++ src/app/controller/api.ts
@@ -1,38 +1,36 @@
-import {Request, Response, Router} from "express";
+import { Request, Response, Router } from "express";
 import db from "../db";
-import {Rule} from "../../entity/rule";
-import {Like} from "typeorm";
+import { Rule } from "../../entity/rule";
+import { Like } from "typeorm";
 
 const router = Router();
 
 router.get("/search", (req: Request, res: Response) => {
   console.log("/search request: ", req.query);
-  const episodes = db
-    .getRepository(Rule)
+  db.getRepository(Rule)
     .findBy({
-      rule: Like(`%${req.query.q}%`)
+      rule: Like(`%${req.query.q}%`),
     })
     .then((rules) => {
-      res.status(200).json(rules)
+      res.status(200).json(rules);
     })
     .catch((err) => {
-      console.log(err)
-      res.status(500).json({})
-    })
+      console.log(err);
+      res.status(500).json({});
+    });
 });
 
-router.get("/rules", (req: Request, res: Response) => {
-
+router.get("/rules", (_: Request, res: Response) => {
   db.getRepository(Rule)
     .find()
     .then((rules) => {
       console.log("# rules loaded", rules.length);
-      res.status(200).json(rules)
+      res.status(200).json(rules);
     })
     .catch((err) => {
-      console.log(err)
-      res.status(500).json({})
-    })
+      console.log(err);
+      res.status(500).json({});
+    });
 });
 
 export default router;
