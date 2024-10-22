e3bf90f99da7de714a4ef5ba0a7277f51f8dfd8f
Bill Vasfaret <125101202+SkullKid2113@users.noreply.github.com>
2024-10-21 20:33:03 -0500

    update name

--- frontend/src/app/my-service.service.ts
+++ frontend/src/app/my-service.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {
+export class MyServiceService {
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
